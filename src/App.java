6577708b5192edec8a0ca78f39e7490eb2d0dad1
Vineet Patel <Super9983@outlook.com>
2022-12-02 18:00:42 -0500

    added hanged men ascii

--- src/App.java
+++ src/App.java
@@ -101,7 +101,53 @@ public class App {
             System.out.println("      | ");
             System.out.println("=========");
         }else if(a == 5){
-            
+            System.out.println("  +---+ ");
+            System.out.println("  |   | ");
+            System.out.println("  O   | ");
+            System.out.println(" /|\\  | ");
+            System.out.println(" /    |");
+            System.out.println("      | ");
+            System.out.println("=========");
+        }else if(a == 4){
+            System.out.println("  +---+ ");
+            System.out.println("  |   | ");
+            System.out.println("  O   | ");
+            System.out.println(" /|\\  | ");
+            System.out.println("      |");
+            System.out.println("      | ");
+            System.out.println("=========");
+        }else if (a == 3){
+            System.out.println("  +---+ ");
+            System.out.println("  |   | ");
+            System.out.println("  O   | ");
+            System.out.println(" /|   | ");
+            System.out.println("      |");
+            System.out.println("      | ");
+            System.out.println("=========");
+        }else if (a == 2){
+            System.out.println("  +---+ ");
+            System.out.println("  |   | ");
+            System.out.println("  O   | ");
+            System.out.println("  |   | ");
+            System.out.println("      |");
+            System.out.println("      | ");
+            System.out.println("=========");
+        }else if (a == 1){
+            System.out.println("  +---+ ");
+            System.out.println("  |   | ");
+            System.out.println("  O   | ");
+            System.out.println("      | ");
+            System.out.println("      |");
+            System.out.println("      | ");
+            System.out.println("=========");
+        }else if (a == 0){
+            System.out.println("  +---+ ");
+            System.out.println("  |   | ");
+            System.out.println("      | ");
+            System.out.println("      | ");
+            System.out.println("      |");
+            System.out.println("      | ");
+            System.out.println("=========");
         }
     }
 
